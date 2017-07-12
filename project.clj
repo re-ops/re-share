cec7134d86fdd680a90e98b0d129b3ec6387262c
ronen <narkisr@gmail.com>
2017-07-13 00:35:27 +1000

    initial import

---
+++ project.clj
@@ -0,0 +1,18 @@
+(defproject re-com "0.1.0"
+  :description "Common utilities for re-ops"
+  :url "https://github.com/re-ops/re-com"
+  :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :dependencies [
+     [org.clojure/clojure "1.8.0"]
+
+
+     ; zeromq
+     [org.zeromq/jzmq "3.1.1-SNAPSHOT"]
+
+     ; fs access
+     [me.raynes/fs "1.4.6"]
+     
+     ; string interpulation  
+     [org.clojure/core.incubator "0.1.4"]
+  ]
+)
---
+++ src/re_com/zero/common.clj
@@ -0,0 +1,37 @@
+(comment
+  re-gent, Copyright 2017 Ronen Narkis, narkisr.com
+  Licensed under the Apache License,
+  Version 2.0  (the "License") you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.)
+
+(ns re-com.zero.common
+  (:require
+    [clojure.core.strint :refer  (<<)]
+    [re-gent.zero.keys :refer (read-key paths)])
+  (:import
+    [org.zeromq ZMQ]
+    ))
+
+(defn context [] (ZMQ/context 1))
+
+(defn close! [sockets]
+  (doseq [[k s] sockets] (.close s)))
+
+(defn client-socket [t parent]
+  (let [{:keys [server-public client-public client-private]} (paths parent)]
+    (doto
+      (.socket (context) t)
+      (.setZAPDomain (.getBytes "global"))
+      (.setCurveServerKey (read-key server-public))
+      (.setCurvePublicKey (read-key client-public))
+      (.setCurveSecretKey (read-key client-private)))))
+
+(comment
+  (read-key ".curve/client-private.key")
+  (alength (read-key ".curve/client-public.key"))
+  )
