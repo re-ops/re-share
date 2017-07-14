7c26cf9f70ec860e987b74df443be42d08bd9ff3
ronen <narkisr@gmail.com>
2017-07-14 23:10:47 +1000

    removing snapshot and adding find-port

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
 (defproject re-share "0.1.0"
   :description "Common utilities for re-ops"
-  :url "https://github.com/re-ops/re-com"
+  :url "https://github.com/re-ops/re-share"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [
      [org.clojure/clojure "1.8.0"]
 
-
      ; zeromq
-     [org.zeromq/jzmq "3.1.1-SNAPSHOT"]
+     [org.zeromq/jzmq "3.1.1"]
 
      ; fs access
      [me.raynes/fs "1.4.6"]
@@ -15,6 +14,8 @@
      ; string interpulation  
      [org.clojure/core.incubator "0.1.4"]
   ]
+
+  :repositories  {"bintray"  "http://dl.bintray.com/content/narkisr/narkisr-jars"}
 
   :signing {:gpg-key "narkisr@gmail.com"}
 )
--- src/re_share/core.clj
+++ src/re_share/core.clj
@@ -1,2 +1,10 @@
 (ns re-share.core)
 
+(defn find-port
+   "find the first available port within a given range"
+   [from to]
+   (first
+     (filter
+       (fn [p] (try (with-open [s (java.net.ServerSocket. p)] true) (catch Exception e false))) (range from to))))
+
+
