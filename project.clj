8a4bbcbcd3a9c51a0958f3754b11e562708ced68
ronen <narkisr@gmail.com>
2017-07-15 23:12:05 +1000

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-share "0.1.0"
+(defproject re-share "0.1.1"
   :description "Common utilities for re-ops"
   :url "https://github.com/re-ops/re-share"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
