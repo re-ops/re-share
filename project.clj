566ab2ee8a0a88b835817de1a0f634b89726e315
ronen <narkisr@gmail.com>
2017-09-19 01:25:52 +1000

    freebsd compatible jna version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-share "0.2.0"
+(defproject re-share "0.2.1"
   :description "Common utilities for re-ops"
   :url "https://github.com/re-ops/re-share"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
@@ -21,8 +21,11 @@
      [fogus/minderbinder "0.2.0"]
 
      ; metrics
-     [com.github.oshi/oshi-core "3.4.2"]
-     [com.github.oshi/oshi-json "3.4.2"]
+     [com.github.oshi/oshi-core "3.4.3" :exclude [net.java.dev.jna/jna net.java.dev.jna/jna-platform]]
+     [com.github.oshi/oshi-json "3.4.3"]
+     [net.java.dev.jna/jna "4.5.0"]
+     [net.java.dev.jna/jna-platform "4.5.0"]
+
      [cheshire "5.7.1"]
   ]
 
