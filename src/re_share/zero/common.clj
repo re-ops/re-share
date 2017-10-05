55f7c2f2e60ef2726411c3237f27d9e9c8441702
ronen <narkisr@gmail.com>
2017-10-05 22:57:27 +1100

    adding close

--- src/re_share/zero/common.clj
+++ src/re_share/zero/common.clj
@@ -1,14 +1,23 @@
 (ns re-share.zero.common
   (:require
    [clojure.core.strint :refer  (<<)]
-   [re-share.zero.keys :refer (read-key paths)])
+   [re-share.zero.keys :refer (read-key paths)]
+   [re-share.core :refer (error-m)]
+   )
   (:import
    [org.zeromq ZMQ]))
 
 (defn context [] (ZMQ/context 1))
 
+(defn close [s]
+  (try
+    (.setLinger s 0)
+    (.close s)
+    (catch Exception e
+      (error-m e))))
+
 (defn close! [sockets]
-  (doseq [[k s] sockets] (.close s)))
+  (doseq [[k s] sockets] (close s)))
 
 (defn client-socket [ctx t parent]
   (let [{:keys [server-public client-public client-private]} (paths parent)]
