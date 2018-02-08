4effe33256687edefa4fd7351e9b5ba2f29eef4d
ronen <narkisr@gmail.com>
2018-02-08 23:43:14 +1100

    removing close

--- src/re_share/schedule.clj
+++ src/re_share/schedule.clj
@@ -56,7 +56,7 @@
    (doseq [[k f] @chs] (halt! k)))
   ([k]
    (debug "closing channel")
-   (close! (@chs k))
+   ((@chs k))
    (debug "clearing chs and status atoms")
    (swap! chs dissoc k)
    (swap! status dissoc k)))
