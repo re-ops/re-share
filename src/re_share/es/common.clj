5484b8393a45e16d34a8714accdbffe6843d2ac2
ronen <narkisr@gmail.com>
2018-03-04 00:24:53 +1100

    cleanup and refactoring adding create

--- src/re_share/es/common.clj
+++ src/re_share/es/common.clj
@@ -10,15 +10,26 @@
 
 ; Common ES functions
 
+; when we reset the connection
+(def reactor-stopped "Request cannot be executed; I/O reactor status: STOPPED")
+
+(defn- ok [resp]
+  (#{200 201} (:status resp)))
+
+(defn- handle-ex [e]
+  (when-not (= (-> e Throwable->map :cause) reactor-stopped)
+    (error e (ex-data e))
+    false))
+
 (defn exists-call
   [target]
   (try
-    (= (:status (s/request @c {:url target :method :head})) 200)
+    (ok (s/request @c {:url target :method :head}))
     (catch Exception e
-      (error e (ex-data e))
-      false)))
+      (handle-ex e))))
 
 (defn exists?
+  "Check if index exists or instance with id existing within an index"
   ([index]
    (exists-call [index]))
   ([index t id]
@@ -27,10 +38,9 @@
 (defn delete-call
   [target]
   (try
-    (= (:status (s/request @c {:url target :method :delete})) 200)
+    (ok (s/request @c {:url target :method :delete}))
     (catch Exception e
-      (error e (ex-data e))
-      false)))
+      (handle-ex e))))
 
 (defn delete
   ([index]
@@ -41,18 +51,16 @@
 (defn delete-all
   [index t]
   (try
-    (= (:status (s/request @c {:url [index t :_delete_by_query] :method :post :body {:query {:match_all {}}}})) 200)
+    (ok (s/request @c {:url [index t :_delete_by_query] :method :post :body {:query {:match_all {}}}}))
     (catch Exception e
-      (error e (ex-data e))
-      false)))
+      (handle-ex e))))
 
 (defn put-call
   [target m]
   (try
-    (= (:status (s/request @c {:url target :method :put :body m})) 200)
+    (ok (s/request @c {:url target :method :put :body m}))
     (catch Exception e
-      (error e (ex-data e))
-      false)))
+      (handle-ex e))))
 
 (defn put [index t id m]
   (put-call [index t id] m))
@@ -62,13 +70,25 @@
     (get-in (s/request @c {:url [index t id] :method :get}) [:body :_source])
     (catch Exception e
       (when-not (= 404 (:status (ex-data e)))
-        (throw e)))))
+        (handle-ex e)))))
+
+(defn create
+  "Persist instance m of type t and return generated id"
+  [index t m]
+  (try
+    (let [{:keys [status body] :as resp} (s/request @c {:url [index t] :method :post :body m})]
+      (when-not (ok m)
+        (throw (ex-info "failed to persist results" resp)))
+      (body :_id))
+      (catch Exception e
+        (handle-ex e))))
 
 (def ^:const settings {:number_of_shards 1})
 
-(defn create
+(defn create-index
+  "Create an index with provided mappings"
   [index mappings]
-  (= (:status (s/request @c {:url [index] :method :put :body mappings})) 200))
+  (ok (s/request @c {:url [index] :method :put :body mappings})))
 
 (defn clear
   "Creates systems index and type"
