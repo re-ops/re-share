fc13cc3d7260f71dcdcb1af188ad400f8f5b3de7
ronen <narkisr@gmail.com>
2018-03-19 00:59:42 +1100

    using connection get function

--- src/re_share/es/common.clj
+++ src/re_share/es/common.clj
@@ -4,7 +4,7 @@
   (:require
    [taoensso.timbre :refer (refer-timbre)]
    [qbits.spandex :as s]
-   [re-share.es.node :refer (c)]))
+   [re-share.es.node :refer (connection)]))
 
 (refer-timbre)
 
@@ -29,7 +29,7 @@
 (defn exists-call
   [target]
   (try
-    (ok (s/request @c {:url target :method :head}))
+    (ok (s/request (connection) {:url target :method :head}))
     (catch Exception e
       (when-not (= 404 (:status (ex-data e)))
         (handle-ex e)))))
@@ -44,7 +44,7 @@
 (defn delete-call
   [target]
   (try
-    (ok (s/request @c {:url target :method :delete}))
+    (ok (s/request (connection) {:url target :method :delete}))
     (catch Exception e
       (handle-ex e))))
 
@@ -57,14 +57,14 @@
 (defn delete-all
   [index t]
   (try
-    (ok (s/request @c {:url [index t :_delete_by_query] :method :post :body {:query {:match_all {}}}}))
+    (ok (s/request (connection) {:url [index t :_delete_by_query] :method :post :body {:query {:match_all {}}}}))
     (catch Exception e
       (handle-ex e))))
 
 (defn put-call
   [target m]
   (try
-    (ok (s/request @c {:url target :method :put :body m}))
+    (ok (s/request (connection) {:url target :method :put :body m}))
     (catch Exception e
       (handle-ex e))))
 
@@ -73,7 +73,7 @@
 
 (defn get [index t id]
   (try
-    (get-in (s/request @c {:url [index t id] :method :get}) [:body :_source])
+    (get-in (s/request (connection) {:url [index t id] :method :get}) [:body :_source])
     (catch Exception e
       (when-not (= 404 (:status (ex-data e)))
         (handle-ex e)))))
@@ -82,7 +82,7 @@
   "Persist instance m of type t and return generated id"
   [index t m]
   (try
-    (let [{:keys [status body] :as resp} (s/request @c {:url [index t] :method :post :body m})]
+    (let [{:keys [status body] :as resp} (s/request (connection) {:url [index t] :method :post :body m})]
       (when-not (ok resp)
         (throw (ex-info "failed to create" {:resp resp :m m :t t :index index})))
       (body :_id))
@@ -94,7 +94,7 @@
 (defn create-index
   "Create an index with provided mappings"
   [index mappings]
-  (ok (s/request @c {:url [index] :method :put :body mappings})))
+  (ok (s/request (connection) {:url [index] :method :put :body mappings})))
 
 (defn clear
   "Creates systems index and type"
@@ -107,13 +107,13 @@
   "An all query using match all on provided type, this should use scrolling for 10K systems"
   [index type]
   (let [query {:size 10000 :query {:match_all {}}}
-        {:keys [body]} (s/request @c {:url [index type :_search] :method :get :body query})]
+        {:keys [body]} (s/request (connection) {:url [index type :_search] :method :get :body query})]
     (mapv (juxt :_id :_source) (get-in body [:hits :hits]))))
 
 (defn delete-by
   "Delete by query like {:match {:type \"nmap scan\"}}"
   [index type query]
   (try
-    (s/request @c {:url [index type :_delete_by_query] :method :post :body {:query query}})
+    (s/request (connection) {:url [index type :_delete_by_query] :method :post :body {:query query}})
     (catch Exception e
       (handle-ex e))))
