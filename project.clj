d566b99d936ad8c3f2c3e2ee29d62e7abd92ea3b
ronen <narkisr@gmail.com>
2018-10-28 00:08:04 +1100

    latest deps and bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-share "0.9.3"
+(defproject re-share "0.9.4"
   :description "Common utilities for re-ops"
   :url "https://github.com/re-ops/re-share"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
@@ -40,7 +40,7 @@
      [narkisr/clansi "1.2.0"]
 
      ; es
-     [zentai "0.1.2"]
+     [zentai "0.1.3"]
 
      ; configuration
      [clj-config "0.2.0" ]
