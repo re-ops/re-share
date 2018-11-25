9f811992d0ab1c184877015c0d18df6ac25e5e15
ronen <narkisr@gmail.com>
2018-11-25 21:33:52 +1100

    bump and adding daily parameter

--- src/re_share/components/elastic.clj
+++ src/re_share/components/elastic.clj
@@ -12,19 +12,21 @@
 
 (defn initialize
   "Creates systems index and types"
-  [parent types]
+  [parent types daily?]
   (doseq [[k t] types]
-    (let [idx (common/day-index parent k)]
+    (let [f (if daily? common/day-index common/index)
+          idx (f parent k)]
       (when-not (exists? idx)
         (info "Creating index" idx)
         (create-index idx {:mappings {k t}}))
-      (clean/setup-index-jobs parent {k t}))))
+      (when daily?
+        (clean/setup-index-jobs parent {k t})))))
 
-(defrecord Elastic [types parent]
+(defrecord Elastic [types parent daily?]
   Lifecyle
   (setup [this]
     (node/connect (get-es!))
-    (initialize parent types))
+    (initialize parent types daily?))
   (start [this]
     (node/connect (get-es!)))
   (stop [this]
