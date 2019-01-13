51cf3b4a3f8f4b315eea8424822b2f4ee963a51f
ronen <narkisr@gmail.com>
2019-01-13 23:34:49 +1100

    moving cleanup logic to common

--- src/re_share/es/common.clj
+++ src/re_share/es/common.clj
@@ -1,7 +1,13 @@
 (ns re-share.es.common
   "Common ES functions"
   (:require
-   [rubber.core :refer (conn-prefix)]
+   [clojure.core.strint :refer (<<)]
+   ; cleanup scheduling
+   [re-share.schedule :refer (watch every-day)]
+   [clj-time.core :as t]
+   [chime :refer [chime-ch]]
+   [rubber.node :as node]
+   [rubber.core :refer (conn-prefix exists? create-index delete-index)]
    [re-share.config :refer (get!)]
    [taoensso.timbre :refer (refer-timbre)]
    [clj-time.core :as t]
@@ -29,3 +35,40 @@
   ([k t d]
    (with-day d (index k t))))
 
+(defn next-index
+  "Create next index for each type"
+  [k mappings]
+  (watch :create-next-day-index (every-day 23)
+         (fn []
+           (let [tommorow (t/plus (t/now) (t/days 1))]
+             (doseq [[t m] mappings]
+               (info "creating new index" (day-index k t tommorow) {:mappings {t m}})
+               (create-index (day-index k t tommorow) {:mappings {t m}}))))))
+
+(defn purge-index
+  "Clear old index from last week"
+  [k mappings]
+  (watch :clear-last-week-index (every-day 23)
+         (fn []
+           (let [last-week (t/minus (t/now) (t/days 7))]
+             (doseq [[t m] mappings
+                     :let [idx (keyword (day-index k t last-week))]]
+               (when (exists? idx)
+                 (info "clearing old index" idx)
+                 (delete-index idx)))))))
+
+(defn setup-index-jobs [k mappings]
+  (next-index k mappings)
+  (purge-index k mappings))
+
+(defn initialize
+  "Creates systems index and types"
+  [parent types daily?]
+  (doseq [[k t] types]
+    (let [f (if daily? day-index index)
+          idx (f parent k)]
+      (when-not (exists? idx)
+        (info "Creating index" idx)
+        (create-index idx {:mappings {k t}}))
+      (when daily?
+        (setup-index-jobs parent {k t})))))
