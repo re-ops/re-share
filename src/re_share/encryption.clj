a0b3b443b06d96eda615fdb12f0fe1ba2eca1a89
narkisr <narkisr@gmail.com>
2019-05-21 23:49:43 +1000

    separating coding an encryption

--- src/re_share/encryption.clj
+++ src/re_share/encryption.clj
@@ -3,7 +3,9 @@
 
      gpg --no-default-keyring --keyring trustedkeys.gpg --fingerprint
 
-   A pulic and private keyrings should be exported (for bountycastle support):
+     gpg --no-default-keyring --keyring trustedkeys.gpg --gen-key
+
+   A public and private keyrings should be exported (for bountycastle support):
 
      gpg --no-default-keyring --keyring trustedkeys.gpg --export >> test/resources/public.gpg
 
@@ -27,26 +29,27 @@
 (defn list-private [k]
   (keyring/list-secret-keys k))
 
-(defn- encode [bs]
+(defn encode [bs]
   (String. (.encode (Base64/getEncoder) bs) "UTF-8"))
 
-(defn- decode [s]
+(defn decode [s]
   (.decode (Base64/getDecoder) s))
 
 (defn encrypt
   "Load public key and encrypt input"
   [input public-key]
   (let [pub (first (load-public public-key))]
-    (encode (msg/encrypt input pub :format :utf8 :cipher :aes-256 :compress :zip))))
+    (msg/encrypt input pub :format :utf8 :cipher :aes-256 :compress :zip)))
 
 (defn decrypt
   "Unlock secret and decrypt encrypted string (key isn't cached)"
   [encrypted secret-key pass]
   (let [k (first (list-private (load-private secret-key)))
         prv (pgp/unlock-key k pass)]
-    (msg/decrypt (decode encrypted) prv)))
+    (msg/decrypt encrypted prv)))
 
 (comment
   (def encrypted (encrypt "this is a secret!" "test/resources/public.gpg"))
+
   (println encrypted)
   (println (decrypt encrypted "test/resources/secret.gpg" "1234")))
