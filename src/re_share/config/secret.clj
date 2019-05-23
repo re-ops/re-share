e7910fdbd20dfaa3fcd94c194bf2649807f84d84
narkisr <narkisr@gmail.com>
2019-05-23 23:26:27 +1000

    saving and loading secrets

--- src/re_share/config/secret.clj
+++ src/re_share/config/secret.clj
@@ -1,33 +1,29 @@
 (ns re-share.config.secret
+  "Secret file managment"
   (:gen-class)
   (:require
    [clojure.java.io :as io]
-   [re-share.encryption :as enc]))
+   [re-share.encryption :as enc :refer (read-pass)]))
 
-(defn read-password
-  "Won't work in the repl because (. System console) is null"
-  []
-  {:pre [(not (nil? (. System console)))]}
-  (apply str (.readPassword (. System console) "Please enter secret store password:")))
+(defn slurp-bytes
+  "Slurp the bytes from a slurpable thing"
+  [f]
+  (with-open [out (java.io.ByteArrayOutputStream.)]
+    (clojure.java.io/copy (clojure.java.io/input-stream f) out)
+    (.toByteArray out)))
 
 (defn load-secrets
-  "Load secret source file from encrypted store and output into /tmp/secrets.edn
-   This cannot be invoked in the repl due to the lack of System console."
-  [source prv]
-  (let [pass (read-password)]
-    (enc/decrypt source prv pass)))
-
-(defn save-secrets
-  "Save /tmp/secrets.edn into an encrypted file"
-  [target pub]
-  (let [input (slurp (io/file "/tmp/secrets.edn"))
-        output (enc/encrypt input pub)]
+  "Load secret source file from encrypted store and output into target
+   This cannot be invoked in the repl due to the lack of System console (see read-pass)"
+  [input target prv]
+  (let [pass (clojure.string/trim (read-pass))
+        output (enc/decrypt (slurp-bytes (io/file input)) prv pass)]
     (spit target output)))
 
-(defn -main [& args]
-  (let [[source prv] args]
-    (load-secrets source prv)))
-
-(comment
-  (save-secrets "secrets" "test/resources/public.gpg")
-  (load-secrets "secrets" "test/resources/secret.gpg"))
+(defn save-secrets
+  "Save secrets.edn into an encrypted file"
+  [input target pub]
+  (let [input (slurp (io/file "/tmp/secrets.edn"))
+        output (enc/encrypt input pub)]
+    (with-open [out (io/output-stream (io/file target))]
+      (.write out output))))
