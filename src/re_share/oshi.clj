647ed9b0b1e88d44f94686576d91b8753a7279df
ronen <narkisr@gmail.com>
2020-01-15 22:50:34 +1100

    using a faster method to obtain os type

--- src/re_share/oshi.clj
+++ src/re_share/oshi.clj
@@ -17,8 +17,18 @@
   ([props]
    (parse-string (.toCompactJSON si props) true)))
 
+(defn linux-type []
+  (into {}
+        (map (fn [line]
+               (let [[f s] (clojure.string/split line #"=")]
+                 [(keyword (clojure.string/lower-case f)) (clojure.string/replace s "\"" "")]))
+             (line-seq (clojure.java.io/reader "/etc/os-release")))))
+
 (defn os []
-  (keyword (get-in (read-metrics) [:operatingSystem :family])))
+  (let [type (keyword (System/getProperty "os.name"))]
+    (case type
+      :Linux (keyword (:name (linux-type)))
+      :default (throw (ex-info "OS type isnt supported" {})))))
 
 (defn get-processes []
   (map bean (.getProcesses (.getOperatingSystem si) 0 nil)))
