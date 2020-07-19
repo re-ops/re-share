c98caf22c50b3cd1b212b22cc384e693fca816b8
ronen <narkisr@gmail.com>
2020-07-19 23:01:48 +1000

    Removing load (moved to re-core)

--- src/re_share/config/core.clj
+++ src/re_share/config/core.clj
@@ -2,24 +2,9 @@
   "Configuration handling"
   (:refer-clojure :exclude  [load])
   (:require
-   [aero.core :as aero]
-   [expound.alpha :as expound]
-   [clojure.spec.alpha :as s]
-   [re-share.config.spec :as cs]
    [clojure.core.strint :refer (<<)]))
 
 (def config (atom nil))
-
-(defn load-config
-  "Load configuration into an Atom (can be called multiple times)"
-  ([]
-   (load-config
-    ::cs/config (<< "~(System/getProperty \"user.home\")/.re-ops.edn") {:profile :dev}))
-  ([spec path profile]
-   (let [c (aero/read-config path profile)]
-     (if-not (s/valid? ::cs/config c)
-       (expound/expound ::cs/config c)
-       (reset! config c)))))
 
 (defn get!
   "Reading a keys path from configuration raises an error of keys not found"
