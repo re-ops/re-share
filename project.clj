d028f70b62bb17c5e1a37bf11afb3516ac099a60
ronen <narkisr@gmail.com>
2021-05-01 20:34:39 +1000

    latest clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/re-ops/re-share"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [
-     [org.clojure/clojure "1.10.1"]
+     [org.clojure/clojure "1.10.3"]
 
      ; zeromq
      [org.zeromq/jeromq "0.5.2"]
